888cfbbcaad8f73aea8cecb566991386a1453de2
dwikurniawan91 <dui.dict@gmail.com>
2019-12-11 19:29:04 +0700

    4.category

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from "react";
 // import Register from "./register/Register";
-// import Login from "./login/Login";
 import Home from "./home/Home";
-
+./components/home/Home
 const App = () => {
   return (
     <div>
